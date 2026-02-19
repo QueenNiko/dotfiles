3338dfb65a312458cb26b34fad7135c801915aed
QueenNiko <marisakirisame945@gmail.com>
2026-02-19 18:33:07 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,8 @@ touch .cache/zsh/HISTORY
 mkdir .config 
 cp -r alacritty hypr lf mako waybar .config/
 
+sudo pacman -S mako hyprland alacritty lf mako mutt waybar \ 
+               firefox element-desktop swaybg swaylock \
+               transmission-cli udiskie rofi grim slurp \
+               vim btop hyfetch \
+               yt-dlp mpv \ 
